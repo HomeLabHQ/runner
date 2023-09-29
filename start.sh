edc94188e804779ca2710a95ecf3ca6e6999e045
Oleksandr Korol <oleksandr.korol@coaxsoft.com>
2023-09-29 17:11:05 +0300

    Add random name

--- start.sh
+++ start.sh
@@ -3,11 +3,12 @@
 ORGANIZATION=$ORGANIZATION
 ACCESS_TOKEN=$ACCESS_TOKEN
 
+
 REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
-./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}  --unattended
+./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}  --name ${hexdump -n 8 -v -e '/1 "%02X"' -e '/16 "\n"' /dev/urandom} --unattended --replace
 
 cleanup() {
     echo "Removing runner..."
