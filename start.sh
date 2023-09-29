065c380a09969ff8365e0ee24e7e6d1a4e6fd6de
Oleksandr Korol <oleksandr.korol@coaxsoft.com>
2023-09-29 17:14:23 +0300

    Fix naming

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.
 
 cd /home/docker/actions-runner
 
-./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}  --name ${hexdump -n 8 -v -e '/1 "%02X"' -e '/16 "\n"' /dev/urandom} --unattended --replace
+./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN} --unattended
 
 cleanup() {
     echo "Removing runner..."
