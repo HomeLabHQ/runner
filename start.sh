75c9c7f04913aa0749dae2a775cc1ae41e9deac1
Oleksandr Korol <oleksandr.korol@coaxsoft.com>
2023-09-29 16:57:43 +0300

    Try multiple instances

--- start.sh
+++ start.sh
@@ -2,13 +2,12 @@
 
 ORGANIZATION=$ORGANIZATION
 ACCESS_TOKEN=$ACCESS_TOKEN
-RUNNER_NAME=$RUNNER_NAME
 
 REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
-./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN} --token $runnerToken --name $RUNNER_NAME --unattended --replace
+./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN} --token $runnerToken --unattended --replace
 
 cleanup() {
     echo "Removing runner..."
